2963e25d076297a47a559224a37864151d027f51
Souvadra <souvadra01@gmail.com>
2021-10-01 07:35:29 -0700

    added a compatible version of Chirag sir's code

--- sub_commands/count_main.cc
+++ sub_commands/count_main.cc
@@ -201,7 +201,6 @@ public:
     if (info < min && l>= w+k) {
       new_min = true;
       if (min != UINT64_MAX) {
-        //if (min.y == info.y) return_mer.push_back(-1);
         return_mer.push_back(-1);
       }
       min = info, min_pos = buf_pos;
@@ -211,7 +210,6 @@ public:
     if (buf_pos == min_pos && if_flag == 0) { // old min has moved outside the window
       new_min = true;
       if (l >= w + k -1 && min != UINT64_MAX) {
-        //if (min.y == info.y) return_mer.push_back(-1);
         return_mer.push_back(-1);
       }
       min = UINT64_MAX;
@@ -224,6 +222,40 @@ public:
   }
   #endif
 
+  // Chirag sir's mimizer code 
+  #if 0
+  void minimizer_helper(uint64_t kmer_int, int z) {
+    uint64_t info = UINT64_MAX;
+    ++l;
+
+    if (l >= k && kmer_span < 256) {
+      info = hash64(kmer_int, mask) << 8 | kmer_span;
+    }
+
+    buf[buf_pos] = info; // need to do this here as appropriate buf_pos and buf[buf_pos] are needed below
+    info_pos = buf_pos;
+
+    int if_flag = 0;
+
+    if (buf[buf_pos] <= min) {
+      min = buf[buf_pos];
+      min_pos = buf_pos;
+      if (l >= w+k-1 && min != UINT64_MAX)
+        return_mer.push_back(-1);
+    } else if (buf_pos == min_pos) { // old min has moved outside the window 
+      // two loops find the rightmost min in casea of tie 
+      for (j = buf_pos + 1, min = UINT64_MAX; j < w; ++j) {
+        if (buf[j] <= min) {min = buf[j], min_pos = j;}
+      } 
+      for (j = 0; j <= buf_pos; ++j) {
+        if (buf[j] <= min) {min = buf[j], min_pos = j;}
+      }
+      if ( l >= w+k-1 && min != UINT64_MAX) 
+        return_mer.push_back(-1);
+    }
+    if (++buf_pos == w) buf_pos = 0;
+  }
+  #endif 
 
   void select_minimizer(uint64_t kmer_int, uint32_t rid, int strand, uint32_t job_id) {
     if ((this->rid != rid) and (this->job_id != job_id)) {
@@ -285,7 +317,7 @@ public:
     MerIteratorType mers(parser_, args.canonical_flag);
     int k = mers->k();
     int w = mers->k(); // this is  hardcoded for now, will need to change it later <-- Souvadra
-    minimizer_factory mmf(k, w); // w value hardcoded, NEET TO CHANGE
+    minimizer_factory mmf(k, w); 
     star_mers_type buf_mer_2[256]; // Souvadra's addition
     star_mers_type min_mer; // Souvadra's addition
     bool min_initialized = 0; // Souvadra's addition
@@ -297,11 +329,8 @@ public:
         if((*filter_)(*mers)) {
           //std::cout << mers->to_str() << " " << "rid: " << mers->get_rid() << "  job id:" << mers->get_job_id() << std::endl;
           mmf.select_minimizer(mers->get_kmer_int(), mers->get_rid(), mers->get_strand(), mers->get_job_id());
-          // std::cout << mers->to_str() << " -- " << mers->get_rid() << " -- " << mers->get_kmer_int() << std::endl; // Souvadra's addition
           buf_mer_2[mmf.info_pos] = *mers; 
-          buf_mer_2[mmf.info_pos].set_kmer_int(mers->get_kmer_int()); // Is it required ???
-          buf_mer_2[mmf.info_pos].set_rid(mers->get_rid()); // Is it required ???    
-          buf_mer_2[mmf.info_pos].set_job_id(mers->get_job_id()); // Is it required ??? 
+          buf_mer_2[mmf.info_pos].set_kmer_int(mers->get_kmer_int()); buf_mer_2[mmf.info_pos].set_rid(mers->get_rid()); buf_mer_2[mmf.info_pos].set_job_id(mers->get_job_id());
           while (!mmf.return_mer.empty()) {
             mer_pos = mmf.return_mer.back();
             if (mer_pos == -1) {
@@ -315,11 +344,8 @@ public:
             } */
             mmf.return_mer.pop_back();
           }
-          if (mmf.new_min) { 
-            min_mer = buf_mer_2[mmf.min_pos]; 
-            min_mer.set_kmer_int(buf_mer_2[mmf.min_pos].get_kmer_int()); // Is it required ???
-            min_mer.set_rid(buf_mer_2[mmf.min_pos].get_rid()); // Is it required ???
-            min_mer.set_job_id(buf_mer_2[mmf.min_pos].get_job_id()); // Is it required ??? 
+          if (m         min_mer = buf_mer_2[mmf.min_pos]; 
+            min_mer.set_kmer_int(buf_mer_2[mmf.min_pos].get_kmer_int()); min_mer.set_rid(buf_mer_2[mmf.min_pos].get_rid()); min_mer.set_job_id(buf_mer_2[mmf.min_pos].get_job_id()); 
             min_initialized = 1; 
           }
         }
